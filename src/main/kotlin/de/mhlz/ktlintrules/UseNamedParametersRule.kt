af5d22b231c4f109cce3a2200d75c3548a3f6c70
Mischa Holz <me@mhlz.de>
2017-04-11 21:28:49 +0200

    :sparkles: Add ignored kotlin std functions

--- src/main/kotlin/de/mhlz/ktlintrules/UseNamedParametersRule.kt
+++ src/main/kotlin/de/mhlz/ktlintrules/UseNamedParametersRule.kt
@@ -10,11 +10,20 @@ import org.jetbrains.kotlin.com.intellij.psi.tree.TokenSet
 /**
  * @author Mischa Holz
  */
+
+val ignoredFunctions = listOf(
+        "listOf", "mapOf", "mutableListOf", "mutableMapOf", "byteArrayOf"
+)
+
 class UseNamedParametersRule : Rule("use-named-parameters") {
     override fun visit(node: ASTNode,
                        autoCorrect: Boolean,
                        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {
         if (node is CompositeElement && node.elementType == CALL_EXPRESSION) {
+            val callText = node.getChildren(TokenSet.create(KtNodeTypes.REFERENCE_EXPRESSION)).firstOrNull()?.text
+            if (callText in ignoredFunctions)
+                return
+
             val argumentList = node.getChildren(TokenSet.create(KtNodeTypes.VALUE_ARGUMENT_LIST))
             val arguments = argumentList.singleOrNull()?.getChildren(TokenSet.create(KtNodeTypes.VALUE_ARGUMENT))
                     ?: return
